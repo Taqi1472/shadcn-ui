5b53789002f8011a26448d37e2a4f9e376f8686a
Syed Muhammad Taqi <smtaqi572@gmail.com>
2024-02-19 14:09:06 +0500

    ok

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,9 +1,9 @@
 import { Products } from "@/utils/mock"
 import { StaticImageData } from "next/image"
-import { Image } from "lucide-react"
+import Image from 'next/image'
 
 const getproductsdetails = (id:string | number) => {
-    return Products.filter((product)=> product.id === id)
+    return Products.filter((product)=> product.id == id)
 }
 
 export default function Page({ params }: { params: { id: string } }) {
@@ -12,12 +12,15 @@ export default function Page({ params }: { params: { id: string } }) {
         <div className='flex flex-col lg:flex-row justify-evenly mt-16 flex-wrap py-10 gap-10'>
           {       
               result.map((product)=>(
-                <div key={product.id} className="flex">
+                <div key={product.id} className="flex gap-10">
                     <div>
 <Image src={product.image} alt={product.name}/>
                     </div>
                     <div>
                         Product Detail
+                        <p>Name {product.name}</p>
+                        <p>Price {product.price}</p>
+                        <p>Category {product.category}</p>
                     </div>
                 </div>
               ))
